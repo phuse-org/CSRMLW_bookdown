4241cab4959234165a7a12d82611d5cad353a967
Kevin Putschko <kevin.putschko@experis.com>
2022-02-11 10:28:06 -0500

    adding helper function for bookdown

---
+++ R/bookdown_helpers.R
@@ -0,0 +1,13 @@
+
+# Custom Functions for Bookdown -------------------------------------------
+
+fn_buildbook <- function(){
+  bookdown::serve_book(
+    dir = ".",
+    output_dir = "docs",
+    preview = F,
+    in_session = T,
+    quiet = F)
+}
+
+# fn_buildbook()
